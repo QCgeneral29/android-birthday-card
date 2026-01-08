9e63b1e3558268f19ce974bc1184804cc4f17c21
Landen <landenlove@protonmail.com>
2026-01-09 08:25:39 +0900

    Added version check for navbar contrast. Extracted birthday text to string resource.

--- app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.birthdaycard
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,10 +30,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        // Allow navigation bar to be transparent
-        // TODO continue tutorial:
-        // https://developer.android.com/codelabs/basic-android-kotlin-compose-add-images?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-1-pathway-3%3F_gl%3D1*1034hmd*_up*MQ..*_ga*NzY5NDU5OTkxLjE3Njc4NDc2NTg.*_ga_6HH9YJMN9M*czE3Njc4NDc2NTckbzEkZzAkdDE3Njc4NDc2NTckajYwJGwwJGgyMTMwMzU3MTAw%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-add-images&_gl=1*1034hmd*_up*MQ..*_ga*NzY5NDU5OTkxLjE3Njc4NDc2NTg.*_ga_6HH9YJMN9M*czE3Njc4NDc2NTckbzEkZzAkdDE3Njc4NDc2NTckajYwJGwwJGgyMTMwMzU3MTAw#4
-        window.setNavigationBarContrastEnforced(false)
+        // Allow navigation bar to be transparent. Requires specific Android version.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.setNavigationBarContrastEnforced(false)
+        }
         setContent {
             BirthdayCardTheme {
                 Surface(
@@ -39,8 +41,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingImage(
-                        message = "Happy Birthday Landen!",
-                        from = "From Brandi"
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text)
                     )
                 }
 
